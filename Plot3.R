21c7567343da733d6c966f328bb41f8da5208e83
Rathod <sush101@users.noreply.github.com>
2014-05-16 11:56:03 +0530

    Create Plot3.R
    This is Plot3.R which is a R file and it creates a plot called Plot3.png

---
+++ Plot3.R
@@ -0,0 +1,49 @@
+# Exploratory Data Analysis:
+# Course Project 1:
+
+# Clean the WorkSpace:
+rm(list=ls(all=TRUE))
+
+# set the working Directory:
+setwd("D:/ExploratoryDataAnalysis/courseProject1/ExData_Plotting1-master")
+
+# Read the text file in R:
+pwrCon<-read.table("household_power_consumption.txt",sep=";",header=T,stringsAsFactors=F)
+pwrCon$Date<-as.Date(pwrCon$Date,"%d/%m/%Y")
+summary(pwrCon)
+dim(pwrCon)
+
+# sub-setting only 1st feb 2007 and 2nd Feb 2007 data:
+pwrConsmptn<-pwrCon[which(pwrCon$Date == '2007-02-01' | pwrCon$Date == '2007-02-02'),]
+summary(pwrCon)
+dim(pwrConsmptn)
+
+pwrConsmptn$DateTime<-paste(pwrConsmptn$Date,pwrConsmptn$Time,sep=" ")
+pwrConsmptn$DateTime<-strptime(pwrConsmptn$DateTime,"%Y-%m-%d %H:%M:%S")
+head(pwrConsmptn,n=3)
+
+# Exploratory Analysis:
+sum(is.na(pwrConsmptn))
+dim(pwrConsmptn)
+summary(pwrConsmptn)
+str(pwrConsmptn)
+
+pwrConsmptn$Global_active_power<-as.numeric(pwrConsmptn$Global_active_power)
+pwrConsmptn$Global_reactive_power<-as.numeric(pwrConsmptn$Global_reactive_power)
+pwrConsmptn$Voltage<-as.numeric(pwrConsmptn$Voltage)
+pwrConsmptn$Sub_metering_1<-as.numeric(pwrConsmptn$Sub_metering_1)
+pwrConsmptn$Sub_metering_2<-as.numeric(pwrConsmptn$Sub_metering_2)
+pwrConsmptn$Time<-strptime(pwrConsmptn$Time,"%H:%M:%S")
+
+head(pwrConsmptn,n=3)
+rm(pwrCon)
+
+# Plot3:
+par(mfrow=c(1,1))
+png("Plot3.png",width=480,height=480)
+with(pwrConsmptn,plot(DateTime,Sub_metering_1,type="l",ylab="Energy Sub metring",xlab=" "))
+with(pwrConsmptn,lines(DateTime,Sub_metering_2,col="red"))
+with(pwrConsmptn,lines(DateTime,Sub_metering_3,col="blue"))
+legend("topright",lty=1,cex=1,col=c("black","red","blue"),legend=c("Sub_metering_1 ","Sub_metering_2 ","Sub_metering_3 "))
+dev.off()
+
